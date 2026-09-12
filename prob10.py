d3def5ad787532d900b2c2598cd5e875896693a0
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-12 23:12:42 +0600

    ok

--- prob10.py
+++ prob10.py
@@ -1,1 +1,16 @@
 # Create a User class with a login() method. Create an Admin class that inherits from User and adds a delete_user() method.
+
+class User:
+    def login(self):
+        print("User logged in")
+
+
+class Admin(User):
+    def delete_user(self):
+        print("User deleted")
+
+
+admin = Admin()
+
+admin.login()
+admin.delete_user()
