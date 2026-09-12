f6c81d9705da939b40c35c00b25d48e2e46da078
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-12 23:11:26 +0600

    prob9

--- prob9.py
+++ prob9.py
@@ -1,1 +1,14 @@
 # Create a Product class with name and price. Create two objects and print their information.
+
+class product:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+
+    def show_info(self):
+        print(self.name,self.price)
+
+product1=product("laptop",5555555)
+product2=product("os",5555)
+
+product1.show_info()
