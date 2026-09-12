550d65d57a26cfa2fbc8abfdbbdd314d3d8511c6
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-12 22:49:44 +0600

    prob4

---
+++ prob4.py
@@ -0,0 +1,9 @@
+# Create a dictionary for a student containing name, age, and marks. Print the student's name and marks.
+
+user={
+    "name":"mehmed",
+    "age":74,
+    "marks":99
+}
+print(user["name"])
+print(user["marks"])
