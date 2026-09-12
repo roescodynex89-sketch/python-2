7c80bc9019da45ee18508130ea36260e55218721
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-12 22:46:09 +0600

    prob

---
+++ prob3.py
@@ -0,0 +1,8 @@
+# Create a tuple containing a person's name, age, and country. Then unpack the values.
+
+person=("smith",49,"Aus")
+
+name, age, country= person
+print(name)
+print(age)
+print(country)
