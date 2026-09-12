6c7fcefd125cad89a003972465382f450371fa28
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-12 23:05:25 +0600

    prob8

--- prob8.py
+++ prob8.py
@@ -1,1 +1,10 @@
 # Create a recursive function to calculate the factorial of a number.
+
+
+def factorial(n):
+    if n==1:
+        return 1
+    
+    return n * factorial (n - 1 )
+
+print(factorial(10))
